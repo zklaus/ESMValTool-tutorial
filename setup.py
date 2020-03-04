dbb14e64870e62ba8c9ab98fbbc4095a4a2eb3ca
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2020-03-04 09:54:41 +0100

    Add versioneer

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 import setuptools
+import versioneer
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
 setuptools.setup(
     name="esmvaltool-tutorial",
-    version="0.0.1",
+    version=versioneer.get_version(),
     author="Klaus Zimmermann",
     author_email="klaus.zimmermann@smhi.se",
     description="A ESMValTool Beginner's Guide",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zklaus/ESMValTool-tutorial",
+    cmdclass=versioneer.get_cmdclass(),
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
